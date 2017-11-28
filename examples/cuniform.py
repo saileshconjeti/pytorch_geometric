2545827986ef14bcf8f1034bffec1380d5c139e0
rusty1s <matthias.fey@tu-dortmund.de>
2017-11-28 10:45:35 +0100

    first cuniform try

--- examples/cuniform.py
+++ examples/cuniform.py
@@ -3,12 +3,107 @@ from __future__ import division, print_function
 import os
 import sys
 
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
 sys.path.insert(0, '.')
 sys.path.insert(0, '..')
 
 from torch_geometric.datasets import Cuniform  # noqa
+from torch_geometric.transforms import CartesianAdj  # noqa
+from torch_geometric.utils import DataLoader  # noqa
+from torch_geometric.nn.modules import SplineConv, Lin  # noqa
 
 path = os.path.dirname(os.path.realpath(__file__))
 path = os.path.join(path, '..', 'data', 'Cuniform')
-train_dataset = Cuniform(path, train=True)
-test_dataset = Cuniform(path, train=False)
+transform = CartesianAdj()
+train_dataset = Cuniform(path, train=True, transform=transform)
+test_dataset = Cuniform(path, train=False, transform=transform)
+
+batch_size = 5
+
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = SplineConv(1, 32, dim=3, kernel_size=5)
+        self.conv2 = SplineConv(32, 64, dim=3, kernel_size=5)
+        self.conv3 = SplineConv(64, 64, dim=3, kernel_size=5)
+        self.conv4 = SplineConv(64, 64, dim=3, kernel_size=5)
+        self.conv5 = SplineConv(64, 64, dim=3, kernel_size=5)
+        self.conv6 = SplineConv(64, 64, dim=3, kernel_size=5)
+        self.lin1 = Lin(64, 128)
+        self.lin2 = Lin(128, 4)
+
+    def forward(self, adj, x):
+        x = F.elu(self.conv1(adj, x))
+        x = F.elu(self.conv2(adj, x))
+        x = F.elu(self.conv3(adj, x))
+        x = F.elu(self.conv4(adj, x))
+        x = F.elu(self.conv5(adj, x))
+        x = F.elu(self.conv6(adj, x))
+        x = F.elu(self.lin1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.lin2(x)
+        return F.log_softmax(x)
+
+
+model = Net()
+if torch.cuda.is_available():
+    model = model.cuda()
+
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+
+def train(epoch):
+    model.train()
+
+    # if epoch == 61:
+    #     for param_group in optimizer.param_groups:
+    #         param_group['lr'] = 0.001
+
+    for data in train_loader:
+        adj, target = data['adj']['content'], data['target']
+        input = torch.ones(target.size(0), 1)
+
+        if torch.cuda.is_available():
+            input, adj, target = input.cuda(), adj.cuda(), target.cuda()
+
+        input, target = Variable(input), Variable(target)
+
+        optimizer.zero_grad()
+        output = model(adj, input)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+
+
+def test(epoch):
+    model.eval()
+
+    correct = 0
+    total = 0
+
+    for data in test_loader:
+        adj, target = data['adj']['content'], data['target']
+        input = torch.ones(target.size(0), 1)
+
+        if torch.cuda.is_available():
+            input, adj, target = input.cuda(), adj.cuda(), target.cuda()
+
+        output = model(adj, Variable(input))
+        pred = output.data.max(1)[1]
+        correct += pred.eq(target).cpu().sum()
+        total += target.size(0)
+
+    print('Epoch:', epoch, 'Test Accuracy:', correct / total)
+
+
+for epoch in range(1, 101):
+    train(epoch)
+    test(epoch)
--- torch_geometric/datasets/cuniform.py
+++ torch_geometric/datasets/cuniform.py
@@ -6,6 +6,8 @@ from torch.utils.data import Dataset
 from .utils.dir import make_dirs
 from .utils.ply import read_ply
 from ..graph.geometry import edges_from_faces
+from ..sparse import SparseTensor
+from .data import Data
 
 
 class Cuniform(Dataset):
@@ -22,14 +24,35 @@ class Cuniform(Dataset):
         self.train = train
         self.transform = transform
 
+        # Process data.
         self.process()
 
+        # Load processed data.
         file = self.training_file if train else self.test_file
-        index, position, target, slice = torch.load(file)
-        print(index.size(), index.type())
-        print(position.size(), position.type())
-        print(target.size(), target.type())
-        print(slice.size(), slice.type())
+        index, position, target, node_slice, edge_slice = torch.load(file)
+        self.index, self.position, self.target = index, position, target.long()
+        self.node_slice, self.edge_slice = node_slice, edge_slice
+
+    def __getitem__(self, i):
+        start, end = self.edge_slice[i], self.edge_slice[i + 1]
+        index = self.index[:, start:end]
+        weight = torch.ones(index.size(1))
+
+        start, end = self.node_slice[i], self.node_slice[i + 1]
+        position = self.position[start:end]
+        target = self.target[start:end]
+
+        n = position.size(0)
+        adj = SparseTensor(index, weight, torch.Size([n, n]))
+        data = Data(None, adj, position, target)
+
+        if self.transform is not None:
+            data = self.transform(data)
+
+        return data.all()
+
+    def __len__(self):
+        return self.node_slice.size(0) - 1
 
     @property
     def _processed_exists(self):
@@ -46,10 +69,11 @@ class Cuniform(Dataset):
         make_dirs(os.path.join(self.processed_folder))
 
         indices = []
-        slices = [0]
+        node_slices = [0]
+        edge_slices = [0]
         positions = []
         targets = []
-        for name in self.test_files:
+        for name in self.training_files:
             print(name)
             file_path = os.path.join(self.raw_folder, name)
             position, face, target = read_ply(file_path)
@@ -57,12 +81,15 @@ class Cuniform(Dataset):
             indices.append(index)
             positions.append(position)
             targets.append(target)
-            slices.append(target.size(0))
+            node_slices.append(node_slices[-1] + target.size(0))
+            edge_slices.append(edge_slices[-1] + index.size(1))
 
         index = torch.cat(indices, dim=1)
         position = torch.cat(positions, dim=0)
         target = torch.cat(targets, dim=0)
-        slice = torch.LongTensor(slices)
+        node_slice = torch.LongTensor(node_slices)
+        edge_slice = torch.LongTensor(edge_slices)
 
-        file_path = os.path.join(self.processed_folder, 'test.pt')
-        torch.save((index, position, target, slice), file_path)
+        file_path = os.path.join(self.processed_folder, 'training.pt')
+        data = (index, position, target, node_slice, edge_slice)
+        torch.save(data, file_path)
